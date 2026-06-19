b31a8c810386119df77cdd95e07179948ce2a03e
Yan Servilha <yanservilha@gmail.com>
2026-06-19 16:31:15 -0300

    Adicionada a classe patron

---
+++ model/Patron.java
@@ -0,0 +1,37 @@
+package JavaLibrary.model;
+
+public class Patron {
+    private int id;
+    private String name;
+    private String contact;
+
+    Patron(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getContact(){
+        return this.contact;
+    }
+
+}
