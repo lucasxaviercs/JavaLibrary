49fa8baace50be87c95e3e5c235788525dffd1b1
Yan Servilha <yanservilha@gmail.com>
2026-06-20 17:44:06 -0300

    Adicionado o addPatron no controller

--- controller/PatronController.java
+++ controller/PatronController.java
@@ -1,5 +1,33 @@
 package controller;
 
+import model.Patron;
+import persistence.FileManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PatronController {
-    
+    private List<Patron> patrons;
+    private int nextId;
+
+    public PatronController(){
+        try{
+            patrons = FileManager.loadPatrons();
+        }catch(IOException e){
+            patrons = new ArrayList<>();
+        }
+
+        nextId = patrons.stream().mapToInt(Patron::getId).max().orElse(0) + 1;
+    }
+
+    public void addPatron(String name, String contact) throws IOException{
+        if(name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("The user's name can not be empty");
+        }
+
+        Patron patron = new Patron(nextId++, name.trim(), contact.trim());
+        patrons.add(patron);
+        FileManager.savePatrons(patrons);
+    }
 }
