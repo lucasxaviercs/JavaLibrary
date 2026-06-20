bed4c77c4445651d4e83784e14582e7bf457be86
Yan Servilha <yanservilha@gmail.com>
2026-06-20 17:56:06 -0300

    Adicionado o editPatron

--- controller/PatronController.java
+++ controller/PatronController.java
@@ -21,6 +21,15 @@ public class PatronController {
         nextId = patrons.stream().mapToInt(Patron::getId).max().orElse(0) + 1;
     }
 
+    public Patron findById(int id){
+        for(Patron patron : patrons){
+            if(patron.getId() == id){
+                return patron;
+            }
+        }
+        return null;
+    }
+
     public void addPatron(String name, String contact) throws IOException{
         if(name == null || name.trim().isEmpty()){
             throw new IllegalArgumentException("The user's name can not be empty");
@@ -28,6 +37,22 @@ public class PatronController {
 
         Patron patron = new Patron(nextId++, name.trim(), contact.trim());
         patrons.add(patron);
+        FileManager.savePatrons(patrons);
+    }
+
+    public void editPatron(int id, String newName, String newContact) throws IOException{
+        Patron patron = findById(id);
+
+        if(patron == null){
+            throw new IllegalArgumentException("User id " + id + " not found.");
+        }
+
+        if(newName == null || newName.trim().isEmpty()){
+            throw new IllegalArgumentException("The user's name can not be empty.");
+        }
+
+        patron.setName(newName.trim());
+        patron.setContact(newContact.trim());
         FileManager.savePatrons(patrons);
     }
 }
