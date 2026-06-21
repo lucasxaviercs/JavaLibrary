e03c7f106565b98792becb05994bbad69c72164c
Yan Servilha <yanservilha@gmail.com>
2026-06-21 11:50:04 -0300

    Adicionado o patrons na janela principal da view

--- view/MainWindow.java
+++ view/MainWindow.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.BookController;
+import controller.PatronController;
 import exception.PersistenceException;
 import javax.swing.*;
 import persistence.FileManager;
@@ -16,8 +17,10 @@ public class MainWindow extends JFrame {
 
         try {
             BookController bookController = new BookController(fileManager);
+            PatronController patronController = new PatronController(fileManager);
             JTabbedPane tabs = new JTabbedPane();
             tabs.addTab("Books", new BooksPanel(bookController));
+            tabs.addTab("Patrons", new PatronsPanel(patronController));
             add(tabs);
         } catch (PersistenceException ex) {
             JOptionPane.showMessageDialog(this, "Could not load data files: " + ex.getMessage(), "Startup Error", JOptionPane.ERROR_MESSAGE);
